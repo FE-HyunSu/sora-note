04a4bb08970d97547cde8bd693ba9600bbef0f4d
fe.hyunsu <fe.hyunsu@gmail.com>
2023-09-12 21:07:45 +0900

    feat : useResetRecoilState 적용. README update.

--- pages/list/index.tsx
+++ pages/list/index.tsx
@@ -14,102 +14,11 @@ interface ListItemT {
 const List = () => {
   const [dataMenuList, setDataMenuList] = useState<ListItemT[]>([]);
   const [isDataListAtom, setDataListAtom] = useRecoilState(dataListAtom);
+  const resetCount = useResetRecoilState(dataListAtom);
 
   const dataReset = () => {
     if (confirm('정말 초기화 하시겠습니까?')) {
-      const resetData = [
-        {
-          id: '1',
-          name: '초코우유',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '2',
-          name: '딸기우유',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '3',
-          name: '레드벨벳',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '4',
-          name: '스윗드림',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '5',
-          name: '바닐라',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '6',
-          name: '레몬라임',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '7',
-          name: '솔티드카라멜',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '8',
-          name: '밀크쉐이크',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '9',
-          name: '피치요거트',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '10',
-          name: '크림브륄레',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '11',
-          name: '말차라떼',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '12',
-          name: '모카',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '12',
-          name: '황치즈',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '13',
-          name: '오레오',
-          totalCount: 0,
-          setCount: 18,
-        },
-        {
-          id: '14',
-          name: '블루베리',
-          totalCount: 0,
-          setCount: 18,
-        },
-      ];
-      setDataListAtom(resetData);
+      resetCount();
       location.reload();
       alert('초기화 되었습니다.');
     } else {
