9edf02277ec9f4ba609653cd4a1a971ade52b8cd
fe.hyunsu <fe.hyunsu@gmail.com>
2023-09-13 20:24:55 +0900

    feat : Modal components delete.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,7 +27,6 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main />
-          <div id="_modal"></div>
           <NextScript />
         </body>
       </Html>
